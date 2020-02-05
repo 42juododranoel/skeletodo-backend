583a8c4e47042406d4f81439fcb6ceb192c09bf7
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2020-02-05 12:49:21 +0300

    Todoissimo → Skeletodo rebranding

--- application/skeletodo/asgi.py
+++ application/skeletodo/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoissimo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skeletodo.settings')
 
 application = get_asgi_application()
