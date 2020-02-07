821af125d6d755c8a479d511c3641f6c3884f1ff
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2020-02-07 19:31:23 +0300

    Add flake8 noqa to settings and manage

--- application/skeletodo/settings.py
+++ application/skeletodo/settings.py
@@ -61,10 +61,10 @@ TEMPLATES = [
 ]
 
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},  # noqa: E501
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},  # noqa: E501
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},  # noqa: E501
 ]
 
 AUTHENTICATION_BACKENDS = [
